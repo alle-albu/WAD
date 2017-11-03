92b3969528f353172338db0829ac7666a93edf11
alle-albu <elaalbu@yahoo.com>
2017-11-03 10:40:44 +0200

    added class database table w/ description & video link: entity+repo

---
+++ src/main/java/dao/AllClassesRepository.java
@@ -0,0 +1,10 @@
+package dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import model.AllClass;
+
+public interface AllClassesRepository extends CrudRepository<AllClass, Integer> {
+	
+}
+
